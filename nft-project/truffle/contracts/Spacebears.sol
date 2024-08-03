8f892d88289b08840fc1eb76d13afdad5272c604
IJLAL <ijlal.tanveer294@gmail.com>
2024-08-03 16:32:09 +0500

    smart contract done and compiled

--- nft-project/truffle/contracts/Spacebears.sol
+++ nft-project/truffle/contracts/Spacebears.sol
@@ -1,41 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Spacebear is ERC721, ERC721URIStorage, Ownable {
-    using Counters for Counters.Counter;
+contract Spacebear is ERC721, Ownable {
+    uint256 private _nextTokenId;
 
-    Counters.Counter private _tokenIdCounter;
-
-    constructor() ERC721("Spacebear", "SBR") {}
+    constructor(address initialOwner)
+        ERC721("Spacebear", "SBR")
+        Ownable(initialOwner)
+    {}
 
     function _baseURI() internal pure override returns (string memory) {
         return "https://ethereum-blockchain-developer.com/2022-06-nft-truffle-hardhat-foundry/nftdata/";
     }
 
-    function safeMint(address to, string memory uri) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
-    }
-
-    // The following functions are overrides required by Solidity.
-
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
     }
 }
