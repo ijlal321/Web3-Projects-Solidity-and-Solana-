05a316828b25f990f5c90bda777cde6a3fa1e940
IJLAL <ijlal.tanveer294@gmail.com>
2024-08-08 21:43:27 +0500

    web integration done

--- Chai Dapp -- Truffle React/client/src/App.jsx
+++ Chai Dapp -- Truffle React/client/src/App.jsx
@@ -4,7 +4,30 @@ import Setup from "./components/Setup";
 import Demo from "./components/Demo";
 import Footer from "./components/Footer";
 
+
+import { useState, useEffect } from "react";
+import Web3 from 'web3';
+import myChai from "./contracts/chai.json";
+
+
 function App() {
+  let web3;
+  const [accounts, setAccounts] = useState([]);
+  const [chaiContract, setChaiContract] = useState(null);
+
+  useEffect(() => {
+    const init = async() => {
+      if (window.ethereum) {
+        web3 = new Web3(window.ethereum)
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        setAccounts(accounts);
+
+        const contractInstance = new web3.eth.Contract(myChai.abi, myChai.networks[11155111].address);
+        setChaiContract(contractInstance);
+      }
+    }
+    init();
+  }, []);
   return (
     <EthProvider>
       <div id="App">
