e2ad82288951786dbbcdec5df60797a513322512
IJLAL <ijlal.tanveer294@gmail.com>
2024-08-10 11:49:51 +0500

    cleaned old code in react

--- Chai Dapp -- Truffle React/client/src/App.jsx
+++ Chai Dapp -- Truffle React/client/src/App.jsx
@@ -1,9 +1,3 @@
-import { EthProvider } from "./contexts/EthContext";
-import Intro from "./components/Intro/";
-import Setup from "./components/Setup";
-import Demo from "./components/Demo";
-import Footer from "./components/Footer";
-
 
 import { useState, useEffect } from "react";
 import Web3 from 'web3';
@@ -16,32 +10,27 @@ function App() {
   const [chaiContract, setChaiContract] = useState(null);
 
   useEffect(() => {
-    const init = async() => {
-      if (window.ethereum) {
-        web3 = new Web3(window.ethereum)
-        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-        setAccounts(accounts);
+    const init = async () => {
+      try {
+        if (window.ethereum) {
+          web3 = new Web3(window.ethereum)
+          const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+          setAccounts(accounts);
 
-        const contractInstance = new web3.eth.Contract(myChai.abi, myChai.networks[11155111].address);
-        setChaiContract(contractInstance);
+          const contractInstance = new web3.eth.Contract(myChai.abi, myChai.networks[11155111].address);
+          setChaiContract(contractInstance);
+        }
+      }
+      catch (error) {
+        alert("install metamask plz");
       }
     }
     init();
   }, []);
   return (
-    <EthProvider>
-      <div id="App">
-        <div className="container">
-          <Intro />
-          <hr />
-          <Setup />
-          <hr />
-          <Demo />
-          <hr />
-          <Footer />
-        </div>
-      </div>
-    </EthProvider>
+    <div>
+
+    </div>
   );
 }
 
