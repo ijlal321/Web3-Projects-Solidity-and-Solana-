dee0f9168e9099a28f1c188b6e770ea4ea950f69
IJLAL <ijlal.tanveer294@gmail.com>
2024-08-13 17:32:00 +0500

    learning done

--- address-feature/learningadd/src/App.js
+++ address-feature/learningadd/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
 import Select from 'react-select';
+import copySVG from "./assests/copy-svgrepo-com.svg";
 
 const CustomOption = (props) => {
   const { data, innerRef, innerProps } = props;
@@ -16,9 +17,12 @@ const CustomOption = (props) => {
     </div>
   );
 };
-var isOtherSelected = false;
+var isMouseCLicked = false;
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const componentRef = useRef(null);
+  
 
   const options = [
     { value: 'apple', label: 'Apple' },
@@ -29,60 +33,83 @@ function App() {
     { value: 'grape', label: 'Grape' },
   ];
 
-  const [isOpen, setIsOpen] = useState(false);
-  const selectClickHandle = () => {
-    console.log("var state: ", isOtherSelected);    
-    console.log("click"); 
-    if (!isOtherSelected) {
-      setIsOpen(true);
+  const selectClickHandle = (event) => {
+
+    if (componentRef.current) {
+      const rect = componentRef.current.getBoundingClientRect();
+      const { clientX, clientY } = event;
+
+      const isInComponent =
+        clientX >= rect.left &&
+        clientX <= rect.right &&
+        clientY >= rect.top &&
+        clientY <= rect.bottom;
+
+      console.log(isInComponent);
+      if (isInComponent == false){
+        setIsOpen(false);
+      }
     }
-    else {
+  }
+
+    const selectBlurHandle = () => {
+      console.log("blur");
       setIsOpen(false);
-      isOtherSelected = false;
     }
-  }
-  const selectBlurHandle = () => {
-    console.log("var state: ", isOtherSelected);    
-    console.log("blur"); 
-    setIsOpen(false);
-  }
-  const selectChangeHandle = (event) => {
-    console.log(event);
-    console.log("var state: ", isOtherSelected);    
-    console.log("change");
-    isOtherSelected = true;
-    setIsOpen(false);
-  }
 
+    const selectkeyHandle = (event) => {
+      console.log("key");
+      if (event.key === 'Enter' || event.keyCode === 13 || event.key === ' ' || event.keyCode === 32) {
+        setIsOpen(true);
+      }
+    };
 
+    const selectMousePress = (event) => {
+      console.log("mouse");
+      setIsOpen(!isOpen);
+    }
 
-
-  return (
-    <div className="App">
-      <div id="outer-container">
-        <select id="fruit" name="fruit"
-          onClick={selectClickHandle}
-          onBlur={selectBlurHandle}
-          onChange={selectChangeHandle}
+    return (
+      <div className="App">
+        <div id="outer-container"
         >
-          <option value="apple" >Apple</option>
-          <option value="banana">Banana</option>
-          <option value="cherry">Cherry</option>
-          <option value="date">Date</option>
-          <option value="fig">Fig</option>
-          <option value="grape">Grape</option>
-        </select>
-      </div>
-      <h3 style={{paddingTop:"200px"}}>currently closed {isOpen ? "true" : "false"}</h3>
-      <div id="outer-container" >
-        <Select
-          options={options}
-          components={{ Option: CustomOption }} // Use custom option component
-        />
-      </div>
+          <select id="fruit" name="fruit"
+            ref={componentRef}
+            onMouseDown={selectMousePress}
+            onClick={selectClickHandle}
+            onBlur={selectBlurHandle}
+            onKeyDown={selectkeyHandle}
+          >
+            <option value="apple" >Apple</option>
+            <option value="banana">Banana</option>
+            <option value="cherry">Cherry</option>
+            <option value="date">Date</option>
+            <option value="fig">Fig</option>
+            <option value="grape">Grape</option>
+          </select>
 
-    </div>
-  );
-}
+          <div className='hidden-copy'
+          >
+            {isOpen && options.map((item, idx) => (
+              <p key={idx} style={{ fontSize: "0.95rem" }}>copy</p>
+            ))}
+          </div>
 
-export default App;
+        </div>
+        <h3 style={{ paddingTop: "200px" }}>currently closed {isOpen ? "true" : "false"}</h3>
+        <div id="outer-container" >
+          <Select
+            options={options}
+            components={{ Option: CustomOption }} // Use custom option component
+          />
+        </div>
+
+
+
+      </div>
+    );
+
+
+  }
+
+  export default App;
