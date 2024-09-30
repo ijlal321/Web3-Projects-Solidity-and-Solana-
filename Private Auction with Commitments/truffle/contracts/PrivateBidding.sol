62bba77dabb4f4e3ecee45e69354b952d04e6c85
IJLAL <ijlal.tanveer294@gmail.com>
2024-09-30 18:24:37 +0500

    smart contract updated

--- Private Auction with Commitments/truffle/contracts/PrivateBidding.sol
+++ Private Auction with Commitments/truffle/contracts/PrivateBidding.sol
@@ -8,30 +8,59 @@ contract BiddingSystem {
         string name;
         uint256 startDate;
         uint256 endDate;
-        string[] options;
     }
 
     struct BidData{
-        uint256 commitment;
-        uint256 message; 
+        bytes32 commitment;
+        uint256 value; 
     }
 
-    address[] private 
-    
-    
-    Biddres;
+    address[] private Bidders;
+
+    mapping(address => BidData) public biddingRecord;
 
     Auction public curAuction;
     uint256 constant RANDOM_NUMBER = 9876543210987654321098765432109876543210;
 
-    mapping(address => BidData) public biddingRecord;
-
-    function createVote(string calldata _name, string[] calldata _options) public {
-        curAuction = Auction(_name, block.timestamp, block.timestamp + 120, _options);
-
+    modifier noBidActive(){
+        require(curAuction.endDate < block.timestamp, "A bid currently is currentyly active, wait 2 minutes to lauunch your own for testing");
+        _;
     }
 
+    modifier bidActive(){
+        require(curAuction.endDate > block.timestamp, "No bid is currently active, make a bid first");
+        _;
+    }
 
+    modifier revealBidTimeActive(){
+        require(curAuction.endDate < block.timestamp, "Bid can be revealed only after bid time has ended");
+        require(curAuction.endDate + 120 > block.timestamp, "Reveal time is 2 minutes after bid time");
+        _;
+    }
 
+    function createVote(string calldata _name) external noBidActive() {
+        curAuction = Auction(_name, block.timestamp, block.timestamp + 120);
+
+        for (uint256 i = 0; i < Bidders.length; i++) {
+            delete biddingRecord[Bidders[i]];
+        }
+        delete Bidders;
+    }
+
+    function Bid(bytes32 _commit) external bidActive() {
+        require(biddingRecord[msg.sender].commitment == bytes32(0), "Already bidded");
+        biddingRecord[msg.sender] = BidData(_commit, 0);
+        Bidders.push(msg.sender);
+    }
+
+    function revealBid(int _randomNumber, uint256 _value) external revealBidTimeActive() {
+        require(getHash(_value, _randomNumber) == biddingRecord[msg.sender].commitment, "Invalid Reveal Value");
+        biddingRecord[msg.sender].value = _value;
+    }
+
+    function getHash(uint _amount, int _randomNumber) pure public returns(bytes32){
+        bytes memory combined = abi.encodePacked(RANDOM_NUMBER, _amount, _randomNumber);
+        return keccak256(combined);
+    }
 
 }
